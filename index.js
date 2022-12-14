70fba3e799703c63b1e996e67dcbc1f7f466613b
Nafis-Fahim50 <nafisf96@gmail.com>
2022-12-14 22:22:13 +0600

    get user order data

--- index.js
+++ index.js
@@ -38,6 +38,19 @@ async function run(){
             const result = await orderCollection.insertOne(order)
             res.send(result);
         })
+
+        app.get('/orders', async(req, res) =>{
+            
+            let query = {};
+            if(req.query.email){
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
     }
     finally{
 
