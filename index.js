65477f1a4583e631e2919d0c8f8be118f3a3a445
Nafis-Fahim50 <nafisf96@gmail.com>
2022-12-14 21:30:17 +0600

    get products data

--- index.js
+++ index.js
@@ -14,7 +14,22 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6llxg7j.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri);
+async function run(){
+    try{
+        const productCollection = client.db('e-commerce').collection('products');
+
+        app.get('/products', async(req,res) =>{
+            const query = {}
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err=>console.error(err));
 
 
 app.get('/', (req,res) =>{
