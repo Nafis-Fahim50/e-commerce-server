e1eef19d3cd75a79031b0bcbf84dbc2bbc7cfa1a
Nafis-Fahim50 <nafisf96@gmail.com>
2022-12-14 21:36:04 +0600

    get specific product data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const cors = require('cors')
 require ('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -23,6 +23,13 @@ async function run(){
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
+        })
+
+        app.get('/products/:id', async(req,res) =>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const product = await productCollection.findOne(query);
+            res.send(product);
         })
     }
     finally{
